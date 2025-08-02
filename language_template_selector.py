3033f3bf0a20b96532f443bc960e625fd82883a4
apuertol <apuertolara@provectus.com>
2025-08-01 21:52:40 -0500

    translate

--- language_template_selector.py
+++ language_template_selector.py
@@ -1,10 +1,47 @@
 from jinja2 import Template
-def select_template (employee_name,employee_country,employee_role,today_date_str,start_date_str,language):
+from langchain_aws import ChatBedrock
+import boto3
+from dotenv import load_dotenv 
+import os 
+
+load_dotenv()
+
+def select_template(employee_name,employee_country,employee_role,today_date_str,start_date_str,language):
+    if language.lower().strip()!='english':
+        REGION_NAME = os.getenv("REGION_NAME", "us-east-1")
+        AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+        AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+        MODEL_NAME = os.getenv("MODEL_NAME")
+        
+        os.environ["AWS_REGION"] = REGION_NAME
+        os.environ["AWS_ACCESS_KEY_ID"] = AWS_ACCESS_KEY_ID
+        os.environ["AWS_SECRET_ACCESS_KEY"] = AWS_SECRET_ACCESS_KEY        
+        
+        
+        bedrock_client = boto3.client("bedrock-runtime", region_name="us-east-1")
+        
+        text_llm = ChatBedrock(
+            model_id=MODEL_NAME,
+            client=bedrock_client,
+            model_kwargs=dict(temperature=0, top_k=0),
+        )
+        
+        prompt_today = f"""Translate the following date from English to {language}:"
+                            Date: {today_date_str}
+                            Return only the translated date. Do not include any explanation or reasoning.
+                        """
+        prompt_enroll = f"""Translate the following date from English to {language}:"
+                            Date: {start_date_str}
+                            Return only the translated date. Do not include any explanation or reasoning.
+                        """
+        
+        today_date_str = text_llm.invoke(prompt_today).content
+        start_date_str = text_llm.invoke(prompt_enroll).content
     
-    if language.lower()=='spanish':
+    if language.lower().strip()=='spanish':
         with open("lang_templates/spanish_template.txt", "r") as file:
             template_str = file.read()
-    elif language.lower()=='english':
+    elif language.lower().strip()=='english':
         with open("lang_templates/english_template.txt", "r") as file:
             template_str = file.read()
     else:
